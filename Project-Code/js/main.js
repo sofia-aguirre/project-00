be3ed400eb9fc619091b37518e463c6060574910
sofia-aguirre <sofiaxaguirre@gmail.com>
2018-08-17 16:32:19 -0700

    more layout, began js

--- Project-Code/js/main.js
+++ Project-Code/js/main.js
@@ -1,5 +1,27 @@
 console.log("Let's get started!");
 
+//call modal----------------
+function callModal () {
+    var modal = document.getElementById('myModal');
+    var img = document.getElementById('1stcarou');
+    var modalImg = document.getElementById("img01");
+    
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+    }
+
+    var span = document.getElementsByClassName("close")[0];
+
+    //  close modal on X
+    span.onclick = function() { 
+        modal.style.display = "none";
+    }
+};
+callModal();
+//end of modal ---------------------
+
+
 //Sticky nav bar when scroll reaches About Me section
 //ADD ONCE you have the function of smooth scroll to #aboutme and THEN onclick, do the following:
 // window.onscroll = function() {stickyNav()};
